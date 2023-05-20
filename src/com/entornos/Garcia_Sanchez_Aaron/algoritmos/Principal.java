793c363030472515b6e83982919ef3fe6d3f4cc3
Aarone sasha <aarongarcia058@gmail.com>
2023-05-20 18:31:37 +0200

    enlaces y clase creado

--- src/com/entornos/Garcia_Sanchez_Aaron/algoritmos/Principal.java
+++ src/com/entornos/Garcia_Sanchez_Aaron/algoritmos/Principal.java
@@ -1,10 +1,40 @@
 package com.entornos.Garcia_Sanchez_Aaron.algoritmos;
 
-public class Principal {
+public class Principal extends algoritmos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Apartado 1 - Crea mediante un bucle un array llamado “numeros” con 5
+		números aleatorios del al 20.*/
+		
+				int[] numeros = new int[5];
+				for (int i = 0; i < numeros.length; i++) {
+					numeros[i] = (int) ((Math.random() * (20 - 1 + 1)) + 1);
+				}
+				
+		/* Apartado 2 - Calcula mediante un bucle el número de fibonacci de los 5 
+		números del array y almacena el resultado en otro array llamado 
+		“resultadoFibonacci”.*/
+				
+		int[] resultadoFibonacci = new int[5];
+		for (int i = 0; i < numeros.length; i++) {
+		resultadoFibonacci[i] = algoritmos.fibonacci(numeros[i]);}
+		
+				
+		/* Apartado 3 - Calcula mediante un bucle el factorial de los 5 números del
+		array y almacena el resultado en otro array llamado “resultadoFactorial” */
+				
+		int[] resultadoFactorial = new int[5];
+		for (int i = 0; i < numeros.length; i++) {
+		resultadoFactorial[i] = (int) algoritmos.factorial(numeros[i]);}
+
+		/* Apartado 4 - Comprueba mediante un bucle si los 5 números del 
+		array son primos o no y almacena el resultado en otro array llamado “resultadoPrimos”.*/
+				
+		boolean[] resultadoPrimos = new boolean[5];
+		for (int i = 0; i < numeros.length; i++) {
+	    resultadoPrimos[i] = algoritmos.Primos(numeros[i]);}
+			}
 
 	}
 
-}
+
--- src/com/entornos/Garcia_Sanchez_Aaron/algoritmos/algoritmos.java
+++ src/com/entornos/Garcia_Sanchez_Aaron/algoritmos/algoritmos.java
@@ -2,4 +2,57 @@ package com.entornos.Garcia_Sanchez_Aaron.algoritmos;
 
 public abstract class algoritmos {
 
+	/**
+	 * Metodo de fibonacci
+	 * Recibe un número por teclado y calcula el fibonacci de dicho número
+	 * @return devuelve el Fibonacci de dicho numero.
+	 **/
+	
+	public static int fibonacci(long numero) {
+		int x = 1, y = 0, z = 0;
+		for (int i = 1; i <= numero; i++) {
+			System.out.print(y + ", ");
+			z = y;
+			y = x;
+			x = x + z;
+		}
+		return y;
+	}
+	
+	/**
+	 * Metodo para calcular el factorial de un numero
+	 * Recibe un numero por teclado y calcula el factorial de dicho número
+	 * @return devuelve el factorial de dicho numero.
+	 **/
+	
+	
+	public static double factorial(long numero) {
+		double total = 1;
+		for (int a = 1; a <= numero; a++) {
+			total = total * a;
+		}
+		return total;
+	}
+	
+	/**
+	 * Metodo para calcular el factorial de un numero
+	 * Recibe un numero por teclado y calcula el factorial de dicho número
+	 * @return devuelve el primo de dicho numero.
+	 **/
+
+	public static boolean Primos(int num) {
+		if (num == 4 || num == 1 || num == 0) {
+			return false;
+		}
+		for (int i = 2; i < num / 2; i++) {
+			if (num % i == 0)
+				return false;
+		}
+		return true;
+	}
+
+
 }
+
+
+
